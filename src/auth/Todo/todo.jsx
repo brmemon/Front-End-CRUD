b61feb0a123e063dc945bbc78041ab902f5bd82d
Muhammad Bilal <bmemon124@gmail.com>
2024-05-07 13:01:47 -0700

    add api,  get api and delete api 80%

--- src/auth/Todo/todo.jsx
+++ src/auth/Todo/todo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../components/navbar/navbar";
 import { Button, TextField } from "@mui/material";
 import "./todo.css";
@@ -6,7 +6,6 @@ import Table from "../../components/table";
 import { ToastContainer, toast } from "react-toastify";
 import Sidebar from "../../components/sidebar/sidebar";
 import axios from "axios";
-import { useEffect } from "react";
 
 const Todo = () => {
   let id = sessionStorage.getItem("id")
@@ -17,59 +16,37 @@ const Todo = () => {
     setInput({ ...input, [name]: value });
   };
 
-  useEffect(() => {
-    const fetch = async () => {
-      await axios.get(`http://localhost:5000/api/todo ${id}`)
-        .then((response) => {
-          console.log(response)
-        })
-    }
-    fetch()
-  }, [])
+
 
   const submit = async () => {
     if (input.title === "" || input.body === "") {
-      toast.error("All Fields Are Required")
+      toast.error("All Fields Are Required");
     } else {
-      if (id) {
-        await axios.post("http://localhost:5000/api/todo/add"), { title: input.title, body: input.body, id: id }
-          .then((response) => {
-            console.log(response);
-          })
+      try {
+        const response = await axios.post("http://localhost:5000/api/todo/add", {
+          title: input.title,
+          body: input.body,
+          id: id
+        });
+        console.log(response);
+        setArray([...array, input])
+        setInput({ title: "", body: "" });
+        toast.success(response.data.message);
+      } catch (err) {
+        // console.error("Error:", err.response.data.message);
+        toast.error(err.response);
       }
-      setArray([...array, input])
-      setInput({ title: "", body: "" });
-      toast.success(response);
     }
   }
 
-  // const submit = async (e) => {
-  //   e.preventDefault();
-  //   await axios
-  //     .post("http://localhost:5000/api/todo/add", input)
-  //     .then((response) => {
-  //       const responseData = response.data.message;
-  //       if (responseData === "Todo Add Successfully") {
-  //         toast.success(responseData);
-  //         setInput({ title: "", body: "" });
-  //       }
-  //       if (input.title === "" || input.body === "") {
-  //         toast.error("All Fields Are Required")
-  //       }
-  //     }).catch((error) => {
-  //       toast.error(error.response.data.message)
-  //     });
-  // };
-
-  // const handleSubmit = () => {
-  //   if (input.title === "" || input.body === "") {
-  //     toast.error("Please Fill Title And Body");
-  //   } else {
-  //     setArray([...array, input]);
-  //     setInput({ title: "", body: "" });
-  //     toast.success("Task Added Successfully");
-  //   }
-  // };
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/todo/${id}`)
+      .then((response) => {
+        setArray(response.data.todo)
+      }).catch((err) => {
+        console.log(err?.response?.data?.message, "error.response.data.message");
+      })
+  }, [])
 
   return (
     <div style={{ width: "100%", height: "100vh" }}>
@@ -101,7 +78,7 @@ const Todo = () => {
           <Button className="todo_butoon" variant="contained" onClick={submit}>
             Add
           </Button>
-          <Table data={array} />
+          <Table data={array} setData={setArray} />
         </div>
       </div>
 
--- src/components/table/index.jsx
+++ src/components/table/index.jsx
@@ -2,25 +2,57 @@ import React from "react";
 import "./table.css";
 import { GiPencil } from "react-icons/gi";
 import { MdOutlineDelete } from "react-icons/md";
+import { Button } from "@mui/material";
+import axios from "axios";
 
-const Table = ({ data }) => {
+const Table = ({ data, setData }) => {
+  let userId = sessionStorage.getItem("id")
+  console.log(userId);
+  const del = async (id) => {
+    try {
+      await axios.delete(`http://localhost:5000/api/todo/delete/${id}`, { data: { id: userId } }).then((response) => {
+        console.log(response.data.message);
+        setData(data.filter(item => item._id !== id));
+      });
+      console.log(`Deleted item with id ${id}`);
+    } catch (error) {
+      console.error("Error deleting item:", error.response.data.message);
+    }
+  }
+
+
   return (
     <div className="main_card_div">
-      {data.map((item) => (
-        <ul className="ul_card">
+      {data.map((item, index) => (
+        <ul key={index} className="ul_card">
           <div className="card">
-            <li className="li_card">
-              <h1 className="card_h1" style={{ color: "#1976d2", fontSize: "30px" }}>
-                {"Title"} <GiPencil />
-              </h1>
-              {item.title}
-            </li>
-            <li className="li_card">
-              <h1 className="card_h1" style={{ color: "#1976d2", fontSize: "30px" }}>
-                {"Body"} <MdOutlineDelete />
-              </h1>
-              {item.body}
-            </li>
+            <div className="name_icon">
+              <li className="li_card">
+                <h1 className="card_h1" style={{ color: "#1976d2", fontSize: "30px" }}>
+                  {"Title"}
+                </h1>
+              </li>
+              <li className="li_card">
+                <Button className="card_h1" style={{ color: "#1976d2", fontSize: "30px", width: "10%" }}>
+                  <GiPencil />
+                </Button>
+              </li>
+            </div>
+            <p style={{ wordBreak: "break-all", overflow: "auto", height: "25%" }}>{item.title}</p>
+
+            <div className="name_icon">
+              <li className="li_card">
+                <h1 className="card_h1" style={{ color: "#1976d2", fontSize: "30px" }}>
+                  {"Body"}
+                </h1>
+              </li>
+              <li className="li_card">
+                <Button onClick={() => del(item._id)} className="card_h1" style={{ color: "#1976d2", fontSize: "30px", width: "10%" }}>
+                  <MdOutlineDelete />
+                </Button>
+              </li>
+            </div>
+            <p style={{ wordBreak: "break-all", overflow: "auto", height: "25%" }}>{item.body}</p>
           </div>
         </ul>
       ))}
